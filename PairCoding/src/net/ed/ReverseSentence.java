95fb4fb97a772dfe7ee31c02a906d8680afc9806
Edward Dupre <edwd42@gmail.com>
2018-07-19 21:36:59 -0400

    add class ReveseSentence

---
+++ PairCoding/src/net/ed/ReverseSentence.java
@@ -0,0 +1,23 @@
+package net.ed;
+
+public class ReverseSentence {
+	
+	public static void main(String[] args) {
+		
+		String sentence = "Hello there World";
+		
+		char letters[] = sentence.toCharArray();
+		int len = letters.length;
+		
+		for(int i = 0; i < letters.length/2; i++, len--) {
+			char tmp = letters[i];
+			letters[i] = letters[len - 1];
+			letters[len - 1] = tmp;
+		}
+		
+		for(char letter : letters) {
+			System.out.print(letter);
+		}
+	}
+
+}
